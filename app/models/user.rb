defc5b0d8d944224f8eef01f5f58d4646c2948fa
Ralph <ralphoburu@gmail.com>
2021-02-18 15:32:22 +0300

    fix the user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  validates :email , presence: true, uniqueness: true, 
-  validates :password, presence: true, lenght: {minimum : 8}
+  validates :email, presence: true, uniqueness: true 
+  validates :password, presence: true, length: { minimum: 8 }
 end
