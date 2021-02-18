d5efdc29ff27cb0bb485ada830be2f0329d43160
chrystalme <afam_ifeanyi@live.com>
2021-02-18 17:47:09 +0200

    form_with and form partials with edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   # def set_user
