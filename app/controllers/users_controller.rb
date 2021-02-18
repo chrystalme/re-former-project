3d1c7490c4cfc5758fe749e3427a6e516da87f0a
Ralph <ralphoburu@gmail.com>
2021-02-18 18:06:20 +0300

    add 'forM_with' '

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
